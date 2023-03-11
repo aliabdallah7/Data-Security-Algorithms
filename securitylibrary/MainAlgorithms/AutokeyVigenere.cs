f6aa7752e6d4659a60f59876d49ea92a062f8376
Ali Abdallah <ali.abdallah43792@gmail.com>
2023-03-11 14:18:36 +0200

    AutokeyVigenere
    Encrypt & Decrypt

--- securitylibrary/MainAlgorithms/AutokeyVigenere.cs
+++ securitylibrary/MainAlgorithms/AutokeyVigenere.cs
@@ -10,17 +10,44 @@ namespace SecurityLibrary
     {
         public string Analyse(string plainText, string cipherText)
         {
+
             throw new NotImplementedException();
+
         }
+
 
         public string Decrypt(string cipherText, string key)
         {
-            throw new NotImplementedException();
+            cipherText = cipherText.ToLower();
+            key = key.ToLower();
+
+            string plainText = "";
+            int j = 0;
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int c = (cipherText[i] - key[j] + 26) % 26 + 'a';
+                plainText += (char)c;
+                key += plainText[j];
+                j++;
+            }
+            return plainText;
         }
 
         public string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            plainText = plainText.ToLower();
+            key = key.ToLower();
+
+            string cipherText = "";
+            int j = 0;
+            for (int i = 0; i < plainText.Length; i++)
+            {
+                int c = (plainText[i] + key[j] - 2 * 'a') % 26 + 'a';
+                cipherText += (char)c;
+                key += plainText[i];
+                j++;
+            }
+            return cipherText;
         }
     }
 }
