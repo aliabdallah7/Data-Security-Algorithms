7f0475d81a9d7dc7d4f7af29688fa980f6391c1b
Sohila Sayed <sohaila20191700306@cis.asu.edu.eg>
2023-03-13 17:12:28 +0200

    RailFence updated

--- securitylibrary/MainAlgorithms/RailFence.cs
+++ securitylibrary/MainAlgorithms/RailFence.cs
@@ -10,17 +10,128 @@ namespace SecurityLibrary
     {
         public int Analyse(string plainText, string cipherText)
         {
-            throw new NotImplementedException();
+            int flag = 0;
+            for (int key = 2; key < plainText.Length; key++)
+            {
+                string pT = plainText.ToUpper();
+
+                var cTList = new List<StringBuilder>();
+                StringBuilder result = new StringBuilder();
+
+                int colNum = 0;
+                while (colNum < key)
+                {
+                    StringBuilder buildCT = new StringBuilder();
+                    cTList.Add(buildCT);
+                    colNum++;
+                }
+
+                int currentLine = 0;
+
+                int dir = 1;
+
+                for (int y = 0; y < pT.Length; y++)
+                {
+                    cTList[currentLine].Append(pT[y]);
+
+                    if (currentLine == 0)
+                        dir++;
+
+                    currentLine += 1;
+                    if (currentLine == key)
+                        currentLine = 0;
+                }
+
+                for (int z = 0; z < key; z++)
+                {
+                    string finalCT = cTList[z].ToString();
+                    result.Append(finalCT);
+                }
+                string ctVal = result.ToString();
+                if (ctVal == cipherText)
+                {
+                    flag = key;
+                    break;
+                }
+            }
+            return flag;
         }
+
 
         public string Decrypt(string cipherText, int key)
         {
-            throw new NotImplementedException();
+            int ctLen = cipherText.Length;
+
+            double ct = (double)(ctLen);
+            ct /= (double)(key);
+            int arrLen = (int)Math.Ceiling((ct));
+
+            char[,] pTRec;
+            pTRec = new char[key, arrLen];
+            int rowNum = 0;
+            int x = 0;
+            while (rowNum < key)
+            {
+                for (int i = 0; i < arrLen; i++)
+                {
+                    if (x != ctLen)
+                    {
+                        pTRec[rowNum, i] = cipherText[x];
+                        x++;
+                    }
+
+                }
+                rowNum++;
+            }
+            string resultPT = "";
+            int lenCT = 0;
+            while (lenCT < arrLen)
+            {
+                for (int k = 0; k < key; k++)
+                {
+                    resultPT += pTRec[k, lenCT];
+                }
+                lenCT++;
+            }
+            return resultPT;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+
+            int ptLen = plainText.Length;
+            char[,] cTbuild;
+            cTbuild = new char[key, ptLen];
+            int colNum = 0;
+            int x = 0;
+            while (colNum < ptLen)
+            {
+                for (int i = 0; i < key; i++)
+                {
+                    if (x != ptLen)
+                    {
+                        cTbuild[i, colNum] = plainText[x];
+                        x++;
+                    }
+
+                }
+                colNum++;
+            }
+            string resultCT = "";
+            int len = 0;
+            while (len < key)
+            {
+                for (int k = 0; k < ptLen; k++)
+                {
+                    if (cTbuild[len, k] != '0')
+                    {
+                        resultCT += cTbuild[len, k];
+                    }
+                }
+                len++;
+            }
+            return resultCT;
         }
     }
 }
+
