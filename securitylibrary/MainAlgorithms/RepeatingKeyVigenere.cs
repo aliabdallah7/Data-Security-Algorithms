7b6f5529122241eaf23c8ce7579dbe1086801852
Ali Abdallah <ali.abdallah43792@gmail.com>
2023-03-14 21:11:56 +0200

    Update Reapeatingkey algo
    Updating the encrypt and decrypt functions to pass the tests correctly

--- securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
+++ securitylibrary/MainAlgorithms/RepeatingKeyVigenere.cs
@@ -37,67 +37,87 @@ namespace SecurityLibrary
 
         public string Decrypt(string cipherText, string key)
         {
+            // Define the alphabet used for encryption
             string alpha = "abcdefghijklmnopqrstuvwxyz";
+
             // Convert the ciphertext and key to lowercase to handle plaintext and ciphertext that contain both uppercase and lowercase letters
             cipherText = cipherText.ToLower();
-            key = key.ToLower();
-            //indk-> index of the keyS char 
-            //indp_> index of the cipherS char
-            int indk, indp;
-            // Initialize variables for the plaintext and the index of the current character in the key
-            string plainText = "";
 
+            int temp0 = 0;
+
+            // If the key length is not equal to the ciphertext length, repeat the key until it matches the length of the ciphertext
+            while (key.Length != cipherText.Length)
+            {
+                key = key + key[temp0];
+                temp0++;
+            }
+
+            //indk-> index of the keyS char 
+            //indp_-> index of the cipherS char
+            int indk, indp_;
+
+            // Initialize an empty string to store the plaintext
+            string plainText = "";
 
             // Iterate over each character in the ciphertext to decrypt it using the key
             for (int i = 0; i < cipherText.Length; i++)
             {
+                // Get the index of the current character in the key and the ciphertext
                 indk = alpha.IndexOf(key[i]);
-                //get the row that is start with the key char to know the distance with PTcahr
-                string temp="";
-                for(int d = 0; d< 26; d++)
-                {
-                    temp =temp+alpha[(indk+d)%26];
-                }
-                indp = temp.IndexOf(plainText[i]);
-                // get the index of the column thats has the pt char
-                int indC = indp - indk;
+                indp_ = alpha.IndexOf(cipherText[i]);
 
-                // get the char 
-                plainText += alpha[indC];
-             
-           
+                // Calculate the index of the decrypted character by subtracting the index of the key character from the index of the ciphertext character, then adding 26 and taking the modulus of 26 to wrap around to the end of the alphabet if necessary
+                int plainChar = (indp_ - indk + 26) % 26;
+
+                // Add the decrypted character to the plaintext string
+                plainText += alpha[plainChar];
             }
-         
-            return plainText;
 
-            
+            // Return the decrypted plaintext
+            return plainText;
         }
+
 
         public string Encrypt(string plainText, string key)
         {
+            // Define the alphabet used for encryption
             string alpha = "abcdefghijklmnopqrstuvwxyz";
+
             // Convert the plaintext and key to lowercase to handle plaintext and ciphertext that contain both uppercase and lowercase letters
             plainText = plainText.ToLower();
-            // Initialize an empty string to store the ciphertext and an index to keep track of the current character in the key
+
+            // Initialize an empty string to store the ciphertext
             string cipherText = "";
-           
+            int temp = 0;
+
+            // If the key length is not equal to the plaintext length, repeat the key until it matches the length of the plaintext
+            while (key.Length != plainText.Length)
+            {
+                key = key + key[temp];
+                temp++;
+            }
+
+            //indk-> index of the keyS char 
+            //indp-> index of the plainS char
             int indk, indp;
+
             // Loop through each character in the plaintext to encrypt it using the key
             for (int i = 0; i < plainText.Length; i++)
             {
+                // Get the index of the current character in the key and the plaintext
                 indk = alpha.IndexOf(key[i]);
                 indp = alpha.IndexOf(plainText[i]);
-                // index of the char in the key and add the number of steps to the PTcolumn 
-                int encryptedChar = (indk + indp ) % 26 ;
+
+                // Calculate the index of the encrypted character by adding the indexes of the key and plaintext characters, then taking the modulus of 26 to wrap around to the beginning of the alphabet if necessary
+                int encryptedChar = (indk + indp) % 26;
 
                 // Add the encrypted character to the ciphertext string
                 cipherText += alpha[encryptedChar];
-               
             }
 
             // Return the encrypted ciphertext
             return cipherText;
-         
         }
+
     }
 }
