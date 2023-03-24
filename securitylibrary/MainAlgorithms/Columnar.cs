6d04d52a7cb74ae9ceac661529cab062f40b1fa2
MohamedAliAsu <97251835+MohamedAliAsu@users.noreply.github.com>
2023-03-24 15:20:13 +0200

    Update Columnar.cs

--- securitylibrary/MainAlgorithms/Columnar.cs
+++ securitylibrary/MainAlgorithms/Columnar.cs
@@ -13,14 +13,86 @@ namespace SecurityLibrary
             throw new NotImplementedException();
         }
 
+        
         public string Decrypt(string cipherText, List<int> key)
         {
-            throw new NotImplementedException();
+            int numRows = (int)Math.Ceiling((double)cipherText.Length / key.Count);
+            char[,] grid = new char[numRows, key.Count];
+
+            int charIndex = 0;
+
+      
+            for (int col = 0; col < key.Count; col++)
+            {
+                
+                int colIndex = key.IndexOf(col+1) ;
+                for (int row = 0; row < numRows; row++)
+                {
+                    if (charIndex < cipherText.Length)
+                    {
+                        grid[row, colIndex] = cipherText[charIndex];
+                    }
+                    else
+                    {
+                        grid[row, colIndex] = 'X';
+                    }
+                    charIndex++;
+                }
+            }
+
+            string plainText = "";
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < key.Count; col++)
+                {
+                    
+                        plainText += grid[row, col];
+                    
+                }
+            }
+
+            return plainText;
         }
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            int numCols = key.Count;
+            int numRows = (int)Math.Ceiling((double)plainText.Length / numCols);
+
+            int numEmptyCells = numRows * numCols - plainText.Length;
+            if (numEmptyCells > 0)
+            {
+                plainText = plainText.PadRight(plainText.Length + numEmptyCells, 'x');
+            }
+
+            char[,] matrix = new char[numRows, numCols];
+            int index = 0;
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numCols; col++)
+                {
+                    matrix[row, col] = plainText[index];
+                    index++;
+                }
+            }
+
+            string cipherText = "";
+            int[] keyArray = key.ToArray();
+            
+                for (int col = 0; col < numCols; col++)
+                {
+                    int colIndex = key.IndexOf(col+1);
+                    
+                    for (int row = 0; row < numRows; row++)
+                    {
+                    cipherText += matrix[row, colIndex];
+                    }
+                
+                }
+
+            return cipherText;
+
+
         }
     }
 }
