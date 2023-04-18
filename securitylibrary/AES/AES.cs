49c8ed6252b6981ff711e62b08e2b5430827a463
Ali Maher <am122964@gmail.com>
2023-04-18 08:50:31 +0200

    encryption modified

--- securitylibrary/AES/AES.cs
+++ securitylibrary/AES/AES.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SecurityLibrary.AES
 {
@@ -7,35 +9,61 @@ namespace SecurityLibrary.AES
     /// </summary>
     public class AES : CryptographicTechnique
     {
-        string[,] sBox = new string[,]
-        {
-            {"63","7c","77","7b","f2","6b","6f","c5","30","01","67","2b","fe","d7","ab","76"},
-            {"ca","82","c9","7d","fa","59","47","f0","ad","d4","a2","af","9c","a4","72","c0"},
-            {"b7","fd","93","26","36","3f","f7","cc","34","a5","e5","f1","71","d8","31","15"},
-            {"04","c7","23","c3","18","96","05","9a","07","12","80","e2","eb","27","b2","75"},
-            {"09","83","2c","1a","1b","6e","5a","a0","52","3b","d6","b3","29","e3","2f","84"},
-            {"53","d1","00","ed","20","fc","b1","5b","6a","cb","be","39","4a","4c","58","cf"},
-            {"d0","ef","aa","fb","43","4d","33","85","45","f9","02","7f","50","3c","9f","a8"},
-            {"51","a3","40","8f","92","9d","38","f5","bc","b6","da","21","10","ff","f3","d2"},
-            {"cd","0c","13","ec","5f","97","44","17","c4","a7","7e","3d","64","5d","19","73"},
-            {"60","81","4f","dc","22","2a","90","88","46","ee","b8","14","de","5e","0b","db"},
-            {"e0","32","3a","0a","49","06","24","5c","c2","d3","ac","62","91","95","e4","79"},
-            {"e7","c8","37","6d","8d","d5","4e","a9","6c","56","f4","ea","65","7a","ae","08"},
-            {"ba","78","25","2e","1c","a6","b4","c6","e8","dd","74","1f","4b","bd","8b","8a"},
-            {"70","3e","b5","66","48","03","f6","0e","61","35","57","b9","86","c1","1d","9e"},
-            {"e1","f8","98","11","69","d9","8e","94","9b","1e","87","e9","ce","55","28","df"},
-            {"8c","a1","89","0d","bf","e6","42","68","41","99","2d","0f","b0","54","bb","16"}
+        private static string[] SBOX = {
+            "63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76",
+            "CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0",
+            "B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15",
+            "04", "C7", "23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75",
+            "09", "83", "2C", "1A", "1B", "6E", "5A", "A0", "52", "3B", "D6", "B3", "29", "E3", "2F", "84",
+            "53", "D1", "00", "ED", "20", "FC", "B1", "5B", "6A", "CB", "BE", "39", "4A", "4C", "58", "CF",
+            "D0", "EF", "AA", "FB", "43", "4D", "33", "85", "45", "F9", "02", "7F", "50", "3C", "9F", "A8",
+            "51", "A3", "40", "8F", "92", "9D", "38", "F5", "BC", "B6", "DA", "21", "10", "FF", "F3", "D2",
+            "CD", "0C", "13", "EC", "5F", "97", "44", "17", "C4", "A7", "7E", "3D", "64", "5D", "19", "73",
+            "60", "81", "4F", "DC", "22", "2A", "90", "88", "46", "EE", "B8", "14", "DE", "5E", "0B", "DB",
+            "E0", "32", "3A", "0A", "49", "06", "24", "5C", "C2", "D3", "AC", "62", "91", "95", "E4", "79",
+            "E7", "C8", "37", "6D", "8D", "D5", "4E", "A9", "6C", "56", "F4", "EA", "65", "7A", "AE", "08",
+            "BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74", "1F", "4B", "BD", "8B", "8A",
+            "70", "3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9", "86", "C1", "1D", "9E",
+            "E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF",
+            "8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16"
         };
 
-
-
-
-        string[,] mixColumnMatrix = new string[,]
+        public string HexToBin(string x)
         {
-            {"02","03", "01", "01"},
-            {"01","02", "03", "01"},
-            {"01","01", "02", "03"},
-            {"03","01", "01", "02"}
+            x = Convert.ToString(Convert.ToInt64(x, 16), 2);
+            x = x.Length < 8 ? (new String('0', 8 - x.Length) + x) : x;
+            return x;
+        }
+        public string addRoundKey(string a, string b)
+        {
+            string res = "";
+            for (int i = 0; i < a.Length; i++)
+            {
+                res += a[i] == b[i] ? '0' : '1';
+            }
+
+            return res;
+        }
+        public string binToHex(string a, int b)
+        {
+            string aR = a[b].ToString() + a[b + 1].ToString() + a[b + 2].ToString() + a[b + 3].ToString();
+            aR = Convert.ToInt32(aR, 2).ToString();
+            aR = aR.Length == 1 ? aR :
+                aR == "10" ? "A" :
+                aR == "11" ? "B" :
+                aR == "12" ? "C" :
+                aR == "13" ? "D" :
+                aR == "14" ? "E" : "F";
+            return aR;
+        }
+
+
+
+        public string[] mixColumnMatrix = {
+        "02", "03", "01", "01",
+        "01", "02", "03", "01",
+        "01", "01", "02", "03",
+        "03", "01", "01", "02"
         };
 
 
@@ -111,14 +139,14 @@ namespace SecurityLibrary.AES
         }
 
 
-        private string[] substituteBytes(string[] array, string[,] sbox)
+        private string[] substituteBytes(string[] array, string[] sbox)
         {
             for (int i = 0; i < 4; i++)
             {
                 string temp = array[i];
                 int row = (int)Convert.ToInt32(temp[0].ToString(), 16);
                 int col = (int)Convert.ToInt32(temp[1].ToString(), 16);
-                array[i] = sbox[row, col];
+                array[i] = sbox[row];
             }
 
             return array;
@@ -217,7 +245,7 @@ namespace SecurityLibrary.AES
                     column1 = RotateWord(column1);
 
                     // Substitute each byte in column1 using an S-box lookup table called sBox.
-                    column1 = substituteBytes(column1, sBox);
+                    column1 = substituteBytes(column1, SBOX);
 
                     // Create a new temporary array called RconColumn by looking up a value from a table called Rcon.
                     string[] RconColumn = new string[4];
@@ -252,6 +280,47 @@ namespace SecurityLibrary.AES
             return keySchedule;
         }
 
+        public int whereMySBox(string a)
+        {
+            int aR = Convert.ToInt32(a[0].ToString(), 16);
+            int aL = Convert.ToInt32(a[1].ToString(), 16);
+            int res = aR * 16 + aL;
+            //Console.WriteLine(res.ToString() + ' ');
+            return res;
+        }
+        private int[,] MixColumns(int[,] s)
+        {
+            for (var c = 0; c < 4; c++)
+            {
+                var a = new int[4];  // 'a' is a copy of the current column from 's'
+                var b = new int[4];  // 'b' is a•{02} in GF(2^8)
+                for (var i = 0; i < 4; i++)
+                {
+                    a[i] = s[i, c];
+                    b[i] = (s[i, c] & 0x80) != 0 ? ((s[i, c] << 1) ^ 0x011b) : (s[i, c] << 1);
+                }
+                // a[n] ^ b[n] is a•{03} in GF(2^8)
+                s[0, c] = (b[0] ^ a[1] ^ b[1] ^ a[2] ^ a[3]); // 2*a0 + 3*a1 + a2 + a3
+                s[1, c] = (a[0] ^ b[1] ^ a[2] ^ b[2] ^ a[3]); // a0 * 2*a1 + 3*a2 + a3
+                s[2, c] = (a[0] ^ a[1] ^ b[2] ^ a[3] ^ b[3]); // a0 + a1 + 2*a2 + 3*a3
+                s[3, c] = (a[0] ^ b[0] ^ a[1] ^ a[2] ^ b[3]); // 3*a0 + a1 + a2 + 2*a3
+            }
+            return s;
+        }
+
+
+        public List<string> twoCharsMatrix(string x)
+        {
+            List<string> strList = new List<string>();
+            string str = x.Split('x')[1];
+            for (int i = 0; i < str.Length; i+=2)
+            {
+                strList.Add(str[i].ToString()+str[i+1].ToString());
+            }
+            
+            return strList;
+        }
+
 
 
 
@@ -259,10 +328,112 @@ namespace SecurityLibrary.AES
         {
             throw new NotImplementedException();
         }
-
         public override string Encrypt(string plainText, string key)
         {
-            throw new NotImplementedException();
+            // Divide the plaintext and key into 2-characters matrices
+            List<string> PT = twoCharsMatrix(plainText);
+            List<string> KT = twoCharsMatrix(key);
+
+            // Convert the matrices to binary
+            List<string> PTBinary = PT.Select(HexToBin).ToList();
+            List<string> KTBinary = KT.Select(HexToBin).ToList();
+
+            // Apply Add Round Key
+            List<string> stateRounded = new List<string>();
+            for (int i = 0; i < 16; i++)
+            {
+                stateRounded.Add(addRoundKey(KTBinary[i], PTBinary[i]));
+            }
+
+            // Apply S-box substitution
+            List<string> sBoxMatrixChanger = stateRounded.Select(whereMySBox).Select(s => SBOX[s]).ToList();
+
+            // Apply Shift Rows
+            List<string> Shifted = new List<string>(16);
+            for (int i = 0; i < 16; i++)
+            {
+                Shifted.Add("");
+            }
+            // Apply Shift Rows
+            List<List<string>> state = new List<List<string>>();
+            for (int i = 0; i < 4; i++)
+            {
+                state.Add(new List<string>());
+                for (int j = 0; j < 4; j++)
+                {
+                    state[i].Add(sBoxMatrixChanger[i * 4 + j]);
+                }
+            }
+
+            for (int i = 1; i < 4; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    state[i].Insert(0, state[i].Last());
+                    state[i].RemoveAt(4);
+                }
+            }
+
+            // Flatten the state matrix into a single list
+            List<string> shifted = state.SelectMany(row => row).ToList();
+
+
+            for (int i = 0; i < 16; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Shifted[i] = sBoxMatrixChanger[i];
+                }
+                else if (i == 5 || i == 9 || i == 13)
+                {
+                    Shifted[i - 4] = sBoxMatrixChanger[i];
+                }
+                else if (i == 3 || i == 7 || i == 11)
+                {
+                    Shifted[i + 4] = sBoxMatrixChanger[i];
+                }
+                else if (i == 10 || i == 14)
+                {
+                    Shifted[i - 8] = sBoxMatrixChanger[i];
+                }
+                else if (i == 2 || i == 6)
+                {
+                    Shifted[i + 8] = sBoxMatrixChanger[i];
+                }
+                else if (i == 15)
+                {
+                    Shifted[i - 12] = sBoxMatrixChanger[i];
+                }
+                else if (i == 1)
+                {
+                    Shifted[i + 12] = sBoxMatrixChanger[i];
+                }
+            }
+
+            // Apply Mix Columns
+            int[,] stateMixed = new int[4, 4];
+            for (int c = 0; c < 4; c++)
+            {
+                for (int r = 0; r < 4; r++)
+                {
+                    stateMixed[r, c] = Convert.ToInt32(Shifted[c * 4 + r], 16);
+                }
+            }
+            int[,] mixedColumns = MixColumns(stateMixed);
+
+            // Convert the mixed state matrix back to a list of hex strings
+            List<string> mixed = new List<string>();
+            for (int c = 0; c < 4; c++)
+            {
+                for (int r = 0; r < 4; r++)
+                {
+                    mixed.Add(mixedColumns[r, c].ToString("X2"));
+                }
+            }
+
+            // Combine the matrix back into a single string and return it
+            return string.Join("", mixed);
         }
+
     }
 }
