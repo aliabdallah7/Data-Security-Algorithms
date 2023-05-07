1c9b55fbddb72abb3cb377774fb853eaaad30a0e
karima-sobhi <karimasobhe010@gmail.com>
2023-05-07 19:59:21 +0300

    Update RSA

--- securitylibrary/RSA/RSA.cs
+++ securitylibrary/RSA/RSA.cs
@@ -8,6 +8,7 @@ namespace SecurityLibrary.RSA
 {
     public class RSA
     {
+        //Function to get (number1 power of number2) mod number3
         public int GetPower(int num1, int num2, int num3)
         {
             int res = 1;
@@ -22,6 +23,7 @@ namespace SecurityLibrary.RSA
         public int Encrypt(int p, int q, int M, int e)
         {
             int n = p * q;
+            // Get (M power(e)) mod n .
             int C = GetPower(M, e, n);
             return C;
         }
@@ -29,7 +31,9 @@ namespace SecurityLibrary.RSA
         public int Decrypt(int p, int q, int C, int e)
         {
             int n = p * q;
+            // Get Multiplicative Inverse of e mod n where n is equal to (p-1)*(q-1) as p , q are prime numbers.
             e = ExtendedEuclidObj.GetMultiplicativeInverse(e, (p-1)*(q-1));
+            // Get c power(e) mod n .
             int M = GetPower(C, e, n);
             return M;
         }
