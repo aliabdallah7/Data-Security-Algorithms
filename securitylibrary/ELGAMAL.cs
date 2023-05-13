eb3d30db5b38fdd593138a5a6611625278a8f09f
MohamedAliAsu <97251835+MohamedAliAsu@users.noreply.github.com>
2023-05-13 17:02:56 +0300

    Update ELGAMAL.cs

--- securitylibrary/ELGAMAL.cs
+++ securitylibrary/ELGAMAL.cs
@@ -47,8 +47,87 @@ namespace SecurityLibrary.ElGamal
 
         public int Decrypt(int c1, int c2, int x, int q)
         {
-            throw new NotImplementedException();
+            // applying the mathematical equation 
+            return (c2 * ModInverse(ModPow(c1, x, q), q)) % q;
+        }
 
+        ///<summary>
+        /// Calculates the modular exponentiation x1^x2 mod mod
+        ///</summary>
+        ///<param name="x1">The base</param>
+        ///<param name="x2">The exponent</param>
+        ///<param name="mod">The modulus</param>
+        ///<returns>The result of the modular exponentiation</returns>
+        private int ModPow(int x1, int x2, int mod)
+        {
+            // Initialize result variable with 1
+            int res = 1;
+
+            // While loop to calculate exponentiation
+            while (x2 > 0)
+            {
+                // Check if the current power of 2 is set in the exponent
+                if ((x2 & 1) == 1)
+                {
+                    // Multiply current result by x1 and take the modulus with respect to mod
+
+                    res = (res * x1) % mod;
+                }
+                // Calculate x1^2 % mod to use in the next iteration of the while loop
+                x1 = (x1 * x1) % mod;
+
+
+                // Shift the bits of x2 to the right by 1 to divide by 2
+                x2 = x2 >> 1;
+            }
+            return res;
+        }
+
+        ///<summary>
+        /// Calculates the modular multiplicative inverse of a modulo n using the extended Euclidean algorithm
+        ///</summary>
+        ///<param name="a">The integer for which to find the inverse</param>
+        ///<param name="n">The modulus</param>
+        ///<returns>The modular multiplicative inverse of a modulo n</returns>
+        private int ModInverse(int x, int mod)
+        {
+            // Initialize four variables temporary, newTemporary, remainder and newRemainder to be used in the extended Euclidean algorithm
+
+            int temporary = 0, newTemporary = 1, remainder = mod, newRemainder = x;
+
+
+            // Extended Euclidean algorithm to calculate the modular multiplicative inverse
+            while (newRemainder != 0)
+            {
+                // Calculate the quotient of remainder and newRemainder
+                int quotient = remainder / newRemainder;
+
+                // Store newTemporary in x temporary variable
+                int tempT = newTemporary;
+
+                // Calculate the new value of newTemporary
+                newTemporary = temporary - quotient * newTemporary;
+
+                // Set temporary to the old value of newTemporary
+                temporary = tempT;
+
+                // Store newRemainder in x temporary variable
+                int tempR = newRemainder;
+
+                // Calculate the new value of newRemainder
+                newRemainder = remainder - quotient * newRemainder;
+
+                // Set remainder to the old value of newRemainder
+                remainder = tempR;
+            }
+
+            // If temporary is negative, add mod to temporary
+            if (!(temporary >= 0))
+            {
+                temporary += mod;
+            }
+
+            return temporary;
         }
     }
 }
